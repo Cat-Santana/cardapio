abe156f0b88d76ae38ae3959fc34e95a00379f9c
Cat-Santana <117064491+Cat-Santana@users.noreply.github.com>
2023-10-03 11:18:19 -0300

    Add files via upload

---
+++ reveal.js
@@ -0,0 +1,8 @@
+ScrollReveal({reset: true});
+
+ScrollReveal().reveal('.info', {duration: 3000});
+
+ScrollReveal().reveal('.pratos-1', {duration: 2000});
+ScrollReveal().reveal('.pratos-2', {duration: 4000});
+ScrollReveal().reveal('.pratos-3', {duration: 4000});
+ScrollReveal().reveal('.pratos-4', {duration: 4000});
